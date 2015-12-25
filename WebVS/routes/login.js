799d11838fc59f1259410abdde3faa5192b9b5d5
Nikolay <NKatkov@yandex.ru>
2015-12-25 15:31:24 +0000

    2

--- WebVS/routes/login.js
+++ WebVS/routes/login.js
@@ -1,7 +1,16 @@
 ﻿var express = require('express');
 var crypto = require('crypto');
 var router = express.Router();
+var mongoose = require('mongoose');
 
+var UserSchema = new mongoose.Schema( {
+    nick: { type: String, default: "hahaha" },
+    pass: { type: String, match: /[a-z]/ },
+    date: { type: Date },
+    buff: Buffer
+} );
+
+var User = dbConnection.model("User",UserSchema)
 
 router.get('/', function (req, res,next) {
     var sess = req.session
@@ -17,7 +26,15 @@ router.get('/', function (req, res,next) {
 router.post('/', function (req, res, next) {
     if (req.body.login && req.body.pass) {
         if (req.body.login == 'admin' && req.body.pass == '1') {
-            //dbConnection
+          var newUser = new User({ name: "Alice", pass: "qwe"})
+	  		newUser.save(function (err, newUser) {
+			if (err){
+				console.log("Something goes wrong with user " + newUser.name);
+			}else{
+				newUser.speak();
+			}
+		});  
+	 //dbConnection
            // console.log(dbConnection.db.sessions.find());
             req.session.user = req.body.login
             res.redirect('/');
