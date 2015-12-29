765a34e0b065cf5178757640616dc2edc3f3d529
Nikolay Katkov <katkov.n@outlook.com>
2015-12-29 15:43:44 +0300

    #1

--- WebVS/routes/login.js
+++ WebVS/routes/login.js
@@ -2,16 +2,6 @@
 var router = express.Router();
 var db = require('../db').db;
 var User = require('../db').User;
-var mongoose = require('mongoose');
-
-var UserSchema = new mongoose.Schema( {
-    nick: { type: String, default: "hahaha" },
-    pass: { type: String, match: /[a-z]/ },
-    date: { type: Date },
-    buff: Buffer
-} );
-
-var User = dbConnection.model("User",UserSchema)
 
 router.get('/', function (req, res, next) {
     var sess = req.session
@@ -24,22 +14,6 @@ router.get('/', function (req, res, next) {
 
 router.post('/auth', function (req, res, next) {
     if (req.body.login && req.body.pass) {
-        if (req.body.login == 'admin' && req.body.pass == '1') {
-          var newUser = new User({ name: "Alice", pass: "qwe"})
-	  		newUser.save(function (err, newUser) {
-			if (err){
-				console.log("Something goes wrong with user " + newUser.name);
-			}else{
-				newUser.speak();
-			}
-		});  
-	 //dbConnection
-           // console.log(dbConnection.db.sessions.find());
-            req.session.user = req.body.login
-            res.redirect('/');
-            return;
-        }
-
         User.authorize(req.body.login, req.body.pass, function (err, User) {
             if (err) {
                 res.render('login', { error: err });
@@ -49,7 +23,6 @@ router.post('/auth', function (req, res, next) {
             }
         })
         return
-
     }
     res.redirect('/login');
 });
